cd2f33c922d17479a2d7e2bf0ebb626ec30851a7
Arturo Sanhueza <arturosanhueza@MacBook-Pro-de-Arturo.local>
2018-02-01 13:43:24 -0300

    imagenes y click para agrandar

---
+++ muro/VisorDeImagenesVC.swift
@@ -0,0 +1,35 @@
+//
+//  VisorDeImagenesVC.swift
+//  muro
+//
+//  Created by Arturo Sanhueza on 01-02-18.
+//  Copyright © 2018 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class VisorDeImagenesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
