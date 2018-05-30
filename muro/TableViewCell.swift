a3ffa0b51c6980406e92448ab11ea399dd924053
Arturo Sanhueza <arturosanhueza@MacBook-Pro-de-Arturo.local>
2018-05-30 12:00:50 -0400

    primer commit de la rama zoomImagenCorrespondiente

---
+++ muro/TableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewCell.swift
+//  SMS
+//
+//  Created by Arturo Sanhueza on 29-06-17.
+//  Copyright © 2017 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+//import ReadMoreTextView
+
+ class TableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelHora: UILabel!
+    @IBOutlet weak var labelLugar: UILabel!
+    @IBOutlet weak var labelSpeaker1: UILabel!
+    @IBOutlet weak var imagenPerfil: UIImageView!
+    @IBOutlet weak var botonFavorito: UIButton!
+    @IBOutlet weak var imagenMargen: UIImageView!
+
+    /// ponentes
+    @IBOutlet weak var labelNombre: UILabel!
+    @IBOutlet weak var labelLugarPersona: UILabel!
+    @IBOutlet weak var labelInstitucion: UILabel!
+    @IBOutlet weak var labelRol: UILabel!
+    
+ //   @IBOutlet weak var infoDetallePatrocinador:ReadMoreTextView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+}
+    
+}
+
+
